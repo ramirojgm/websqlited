a3622207714b316e6774e53e5fb11b16275cb5bc
ramirojgm <ramiro.jgmp@gmail.com>
2017-03-08 20:49:37 -0600

    add config.h

--- config.h
+++ config.h
@@ -1,9 +1,9 @@
 /*
-	Copyright (C) 2017 Ramiro Jose Garcia Moraga
+	Copyright (C) 2016 Ramiro Jose Garcia Moraga
 
 	This file is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
+	the Free Software Foundation, either version 2 of the License, or
 	(at your option) any later version.
 
 	This file is distributed in the hope that it will be useful,
@@ -15,14 +15,14 @@
 	along with the this software.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <glib.h>
-#include <gwebsocket/gwebsocketservice.h>
-#include "config.h"
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define WEBSQLITE_CONFIG_DIR "./etc/websqlite/"
+#define WEBSQLITE_LOG_DIR "./var/websqlite/log/"
+#define WEBSQLITE_DATA_DIR "./var/websqlite/data/"
+#define WEBSQLITE_CONTROLLER_DIR "./var/websqlite/controller/"
+#define WEBSQLITE_WEB_DIR "./var/websqlite/www/"
 
 
-gint main (gint argc,gchar * argv[])
-{
-
-
-  return 0;
-}
+#endif /* CONFIG_H_ */
